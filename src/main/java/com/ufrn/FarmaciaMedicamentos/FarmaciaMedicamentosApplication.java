5faef2381a797c62ea01d3773f4a9cd52dde104a
Paloma Eduarda <palomaaeduardapro23@gmail.com>
2025-07-07 19:59:04 -0300

    Initial commit

---
+++ src/main/java/com/ufrn/FarmaciaMedicamentos/FarmaciaMedicamentosApplication.java
@@ -0,0 +1,13 @@
+package com.ufrn.FarmaciaMedicamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaciaMedicamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaciaMedicamentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ufrn/FarmaciaMedicamentos/FarmaciaMedicamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufrn.FarmaciaMedicamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaciaMedicamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
